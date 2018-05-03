30fcdf27a3bdc67131785310714ccd73e8959416
Andrej <andrej.byra@centrum.sk>
2018-05-03 18:29:28 +0200

    deleted admin

--- src/main/java/sk/bazos/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package sk.bazos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.bazos.model.Admin;
-
-public interface AdminRepository extends JpaRepository <Admin, Long> {
-}
