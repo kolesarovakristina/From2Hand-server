b2b3a1a853869ddf2386a5393da9a1e828dca6a6
my <my@email.com>
2018-05-09 22:36:50 +0200

    comment

--- src/main/java/sk/bazos/model/Category.java
+++ src/main/java/sk/bazos/model/Category.java
@@ -13,7 +13,7 @@ public class Category implements Serializable {
     @GeneratedValue
     private Long id;
     private String title;
-    @Lob
+    @Lob //ZLE, to, co ti tam Kristina nacrtla bolo dobre, akurat stacilo definovat vazbu lazy load
     private byte[] photoData;
 
     @ManyToOne
