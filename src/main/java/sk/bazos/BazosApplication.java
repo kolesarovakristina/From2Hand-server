9433f59f2880513b27e693c8739c550dfbc754b9
dominiknovicky <dominik.novicky24@gmail.com>
2018-05-16 08:39:51 +0200

    server updated

--- src/main/java/sk/bazos/BazosApplication.java
+++ src/main/java/sk/bazos/BazosApplication.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 public class BazosApplication {
 
-   // public static void main(String[] args) {
+   public static void main(String[] args) {
         SpringApplication.run(BazosApplication.class, args);
     }
 
